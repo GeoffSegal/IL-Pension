53de47dae05c5a44b7978a39d7acbc53e47ef67b
Geoff Segal <geoffsegal3@gmail.com>
2018-07-24 19:12:41 -0500

    delete build3 folder

--- build3/stepper.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $("a.step-link").click(function(e) {
-    var clickedStep = $(this).attr('id');
-    switchStep(clickedStep);
-    switchAnnotation(clickedStep);
-    return false;
-  });
-});
