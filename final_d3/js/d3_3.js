883848db9c660a0a1f9e98ee984b30d5a4097ec4
GeoffSegal <geoffsegal3@gmail.com>
2018-09-26 09:30:42 -0500

    Update d3_3.js

--- final_d3/js/d3_3.js
+++ final_d3/js/d3_3.js
@@ -267,7 +267,6 @@ var parseTime = d3.timeParse("%Y")
         .scale(5000)
         .translate([-200+width / 2, 200+height / 2]);
 		
-	projection.rotate([45,0]);
     
     var zoom = d3.zoom()
         .scaleExtent([1, 8])
@@ -293,7 +292,8 @@ var parseTime = d3.timeParse("%Y")
     //     .attr("height", height)
     //     .on("click", reset);
     
-    var g = svg.append("g");
+    var g = svg.append("g")
+     .transform('rotate('+45+')');
     
     //svg.call(zoom); // delete this line to disable free zooming
     
